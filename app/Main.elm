c26c43da60e77a7630b806caf82809bf763751ca
Tom Harding <tomjharding@live.co.uk>
2016-05-12 11:58:21 +0100

    Helping Joe

--- app/Main.elm
+++ app/Main.elm
@@ -1,7 +1,9 @@
 import Html exposing (Html, div, header)
 import Html.App as Html
 
-import Navigation exposing (model, view)
+import Navigation exposing (view)
+
+-- Fancy types, hwre.
 
 type Dialog
   = DatabaseSchema
@@ -10,13 +12,33 @@ type Dialog
 
 type Status = Red | Amber | Green
 
+-- Application state.
+
+type alias Model =
+  { DatabaseStatus      : Status
+  , CurrentExportStatus : Status
+  , SystemUpdateStatus  : Status
+  }
+
+-- Application actions.
+
+type Action
+  = BuggerAll
+
+-- Application updater.
+
+update : Action -> Model -> Model
+update action model = model
+
+-- Application view.
+
 view : Model -> Html
 view model =
   div
     [ class "thoth-container" ]
     [ header
         []
-        [ Navigation.view
+        [ Navigation.view ]
       ]
     ]
 
