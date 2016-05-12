2e24b4b49554edc86c44e5e3f608faa8fed8d99b
Tom Harding <tomjharding@live.co.uk>
2016-05-12 11:49:14 +0100

    More fixes to Joe's code

--- app/Main.elm
+++ app/Main.elm
@@ -1,4 +1,28 @@
-import Html exposing (text)
+import Html exposing (Html, div, header)
+import Html.App as Html
+
+import Navigation exposing (model, view)
+
+type Dialog
+  = DatabaseSchema
+  | DerivedFields
+  | UpdateSystem
+
+type Status = Red | Amber | Green
+
+view : Model -> Html
+view model =
+  div
+    [ class "thoth-container" ]
+    [ header
+        []
+        [ Navigation.view
+      ]
+    ]
 
 main =
-  text "Hello, World!"
+  Html.beginnerProgram
+      { model  = model
+      , view   = view
+      , update = update
+      }
